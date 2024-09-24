1419779a028e3435cd87e682351c39b97d94d8b8
Shahzeb13 <razashahzaib119@gmail.com>
2024-09-24 12:08:01 +0500

    Update WithoutCreationPrinciple.java

--- SDA/LABS/LAB1-CreationPrinciple/Low Coupling/WithoutCreationPrinciple.java
+++ SDA/LABS/LAB1-CreationPrinciple/Low Coupling/WithoutCreationPrinciple.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softprinciple2;
 
 /**
@@ -10,21 +5,24 @@ package softprinciple2;
  * @author sp22-bse-073
  */
 class Car2 {
-    private String model;
+    private String model; // 'model' variable mein car ka naam ya model store kiya jata hai
     
+    // Constructor jo object banate waqt car ka model set karta hai
     public Car2(String model) {
         this.model = model;
     }
     
+    // Method jo car ko "drive" karte waqt message print karta hai
     public void drive() {
-        System.out.println("Driving a " + model);
+        System.out.println("Driving a " + model); // Yeh car ka model output karta hai
     }
 }
 
-// Main class (without using Creator principle)
+// Main class (yahaan creator principle use nahi kiya gaya)
 public class WithoutCreationPrinciple {
     public static void main(String[] args) {
-        Car2 car = new Car2("Toyota");
-        car.drive();
+        // Direct Car2 object banate hain bina factory ke
+        Car2 car = new Car2("Toyota"); // Car ka object manually create kiya gaya
+        car.drive(); // Car ko "drive" karte hain aur output dikhate hain
     }
 }
