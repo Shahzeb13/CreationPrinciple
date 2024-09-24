b0bb7627244e2df552335311a24151d301a6f1d5
Shahzeb13 <razashahzaib119@gmail.com>
2024-09-24 12:07:04 +0500

    Update WithCreationPrinciple.java

--- SDA/LABS/LAB1-CreationPrinciple/Low Coupling/WithCreationPrinciple.java
+++ SDA/LABS/LAB1-CreationPrinciple/Low Coupling/WithCreationPrinciple.java
@@ -6,29 +6,30 @@
 package softprinciple2;
 
 class Car {
-    private String model;
+    private String model; // Yeh private variable 'model' car ka naam ya model store karta hai
     
+    // Constructor jo 'Car' ka object banane ke waqt model set karta hai
     public Car(String model) {
         this.model = model;
     }
     
+    // Method jo car ko "drive" karne ka message print karta hai
     public void drive() {
-        System.out.println("Driving a " + model);
+        System.out.println("Driving a " + model); // Yeh car ka model screen par dikhata hai
     }
 }
-
 
 class CarFactory {
+    // Method jo 'Car' ka naya object return karta hai jab model pass kiya jaye
     public Car createCar(String model) {
-        return new Car(model);
+        return new Car(model); // Naya car object return karta hai model ke sath
     }
 }
-
 
 public class WithCreationPrinciple {
     public static void main(String[] args) {
-        CarFactory factory = new CarFactory();
-        Car car = factory.createCar("Toyota");
-        car.drive();
+        CarFactory factory = new CarFactory(); // CarFactory ka naya object banate hain
+        Car car = factory.createCar("Toyota"); // CarFactory se 'Toyota' model ki car banate hain
+        car.drive(); // Car ko "drive" karte hain aur message print hota hai
     }
 }
